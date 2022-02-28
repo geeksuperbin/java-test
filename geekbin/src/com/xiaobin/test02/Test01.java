821531242db3b828f2bf8bc3871351de3bec0ca9
geeksuperbin <liubindashen@gmail.com>
2022-02-28 13:32:02 +0800

    java test

--- geekbin/src/com/xiaobin/test02/Test01.java
+++ geekbin/src/com/xiaobin/test02/Test01.java
@@ -1,4 +1,20 @@
 package com.xiaobin.test02;
 
 public class Test01 {
+    public static void main(String[] args) {
+        Animal a = new Tiger();
+        a.run();
+    }
+}
+
+class Tiger extends Animal{
+    @Override
+    public void run(){
+        System.out.println("老虎正在跑");
+    }
+}
+
+
+abstract class Animal{
+    public abstract void run();
 }
