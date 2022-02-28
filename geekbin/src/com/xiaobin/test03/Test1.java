d14ffd50a3044d1547ff15b612e432d40aed6796
geeksuperbin <liubindashen@gmail.com>
2022-02-28 20:40:04 +0800

    java test

---
+++ geekbin/src/com/xiaobin/test03/Test1.java
@@ -0,0 +1,11 @@
+package com.xiaobin.test03;
+
+/**
+ *
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        Student s = new Student("周",'男',19);
+        System.out.printf(s.toString());
+    }
+}
