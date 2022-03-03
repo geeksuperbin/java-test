6a9d24ae52fbc08406c8fb5391559118ad271f15
geekbin <geekbin@163.com>
2022-03-03 22:19:09 +0800

    java test

--- geekbin/src/com/xiaobin/test03/Test4.java
+++ geekbin/src/com/xiaobin/test03/Test4.java
@@ -2,8 +2,9 @@ package com.xiaobin.test03;
 
 public class Test4 {
     public static void main(String[] args) {
-        StringBuilder a = new StringBuilder("abcabcabcacbacbabcabc");
-
+        StringBuilder a = new StringBuilder();
+        a.append("geekbin");
+        a.append("我爱你 中国");
         System.out.println(a);
     }
 }
