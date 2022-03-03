d4c9f0de6abce37cf160c07c6f6de1cedd93b3cf
geeksuperbin <liubindashen@gmail.com>
2022-03-03 17:39:55 +0800

    java test

---
+++ geekbin/src/com/xiaobin/test03/Test4.java
@@ -0,0 +1,9 @@
+package com.xiaobin.test03;
+
+public class Test4 {
+    public static void main(String[] args) {
+        StringBuilder a = new StringBuilder("abcabcabcacbacbabcabc");
+
+        System.out.println(a);
+    }
+}
