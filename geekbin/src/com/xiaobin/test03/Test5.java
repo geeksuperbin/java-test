86afcdd48a3cfb0779148f14ca74fe87699e2a84
geekbin <geekbin@163.com>
2022-03-06 16:36:21 +0800

    java test

--- geekbin/src/com/xiaobin/test03/Test5.java
+++ geekbin/src/com/xiaobin/test03/Test5.java
@@ -9,19 +9,23 @@ public class Test5 {
 
     public static void main(String[] args){
         int[] a = {11,22,33,44,55};
+        // int[] a = null;
         System.out.println(zhengxin(a));
     }
 
     public static String zhengxin(int[] a){
+        if (a == null) return null;
         StringBuilder sb = new StringBuilder();
         sb.append("该数组内容为：[");
         for (int i = 0; i < a.length; i++) {
-            if(i == a.length -1){
-                sb.append(a[i]);
-            }else{
-                sb.append(a[i]).append(",");
-
-            }
+            // if(i == a.length -1){
+            //     sb.append(a[i]);
+            // }else{
+            //     sb.append(a[i]).append(",");
+            //
+            // }
+            // 三元运算符代替上面的 if - else 写法
+            sb.append(a[i]).append(i == a.length - 1 ? "" : ", ");
         }
         sb.append("]");
         return sb.toString();
